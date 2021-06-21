fa52513bf1ab1ab465b8736d3e190a7ea588930c
tanishkul <tanishkul@gmail.com>
2021-06-21 19:09:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './App.css';
 
 function App() {
@@ -5,13 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
